b3dbda440d6d873b7d4e4a41e87fd2a7164a7908
tsdcloud <silatchomsiaka@gmail.com>
2024-02-09 11:52:48 +0100

    Minor bug fixed in request.serializers.py

--- request/serializers.py
+++ request/serializers.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from request.models import Request, Country, Court, Agent, Municipality, Region, Department, Shipment
+from request.models import Request, Service, Country, Court, Agent, Municipality, Region, Department, Shipment
 
 
 class RequestSerializer(serializers.ModelSerializer):

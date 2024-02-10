ed898494b8cabb0837b7f11674260bec6c58068f
tsdcloud <silatchomsiaka@gmail.com>
2024-02-10 16:57:02 +0100

    Test

--- request/utils.py
+++ request/utils.py
@@ -320,14 +320,13 @@ def checkout(request, *args, **kwargs):
     headers['X-Target-Environment'] = 'production'
     headers['Accept-Language'] = 'en'
     headers['Content-Type'] = 'application/json'
-    requests.post(url, headers=headers, data=data)
-    # response = requests.post(url, headers=headers, data=data)
-    # json_string = response.content
-    # json_response = json.loads(json_string)
-    # if json_response['success']:
-    #     pay_token = json_response['pay_token']
-    #     payment.pay_token = pay_token
-    #     payment.save()
+    response = requests.post(url, headers=headers, data=data)
+    json_string = response.content
+    json_response = json.loads(json_string)
+    if json_response['success']:
+        pay_token = json_response['pay_token']
+        payment.pay_token = pay_token
+        payment.save()
     # except:
     #     logger.error(f"Init payment {payment.id} failed", exc_info=True)
 

b120aa15ac13d5d53c31d8bdf9ea75829dab35d8
tsdcloud <silatchomsiaka@gmail.com>
2024-03-30 17:04:36 +0100

    Update migration

--- request/migrations/0044_alter_agent_managers_remove_service_dispursement_and_more.py
+++ request/migrations/0044_alter_agent_managers_remove_service_dispursement_and_more.py
@@ -32,9 +32,9 @@ class Migration(migrations.Migration):
             name='id',
             field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
         ),
-        # migrations.AlterField(
-        #     model_name='agent',
-        #     name='username',
-        #     field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=False, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
-        # ),
+        migrations.AlterField(
+            model_name='agent',
+            name='username',
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=False, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
+        ),
     ]

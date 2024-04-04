e5a82516f78cf8ca0b5a80a12e2b53253458b3bf
tsdcloud <silatchomsiaka@gmail.com>
2024-04-04 18:55:54 +0200

    Replace permission_classes on RequestViewSet

--- request/views.py
+++ request/views.py
@@ -54,7 +54,7 @@ class RequestViewSet(viewsets.ModelViewSet):
     """
     queryset = Request.objects.all()
     serializer_class = RequestListSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAnonymous]
     # required_groups = {
     #     'GET': ['courierAgents', 'regionalAgents'],
     #     'PATCH': ['regionalAgents']

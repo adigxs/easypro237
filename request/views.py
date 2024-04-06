6cd2b4f387f97b4f83053ad057bb65761d25b772
tsdcloud <silatchomsiaka@gmail.com>
2024-04-06 08:10:07 +0200

    Update permissions in Agent.

--- request/views.py
+++ request/views.py
@@ -54,7 +54,7 @@ class RequestViewSet(viewsets.ModelViewSet):
     """
     queryset = Request.objects.all()
     serializer_class = RequestListSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     # required_groups = {
     #     'GET': ['courierAgents', 'regionalAgents'],
     #     'PATCH': ['regionalAgents']
@@ -90,14 +90,14 @@ class RequestViewSet(viewsets.ModelViewSet):
         else:
             return RequestSerializer
 
-    # def get_permissions(self):
-    #     permission_classes = []
-    #     if self.action == 'list':
-    #         # permission_classes = [HasCourierAgentPermission, HasRegionalAgentPermission, IsAdminUser]
-    #         permission_classes = [IsSudo]
-    #     if self.action == 'partial_update':
-    #         permission_classes = [HasGroupPermission, IsAdminUser, IsAnonymous]
-    #     return [permission() for permission in permission_classes]
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list':
+            # permission_classes = [HasCourierAgentPermission, HasRegionalAgentPermission, IsAdminUser]
+            permission_classes = [permissions.IsAuthenticated]
+        if self.action == 'partial_update':
+            permission_classes = [HasGroupPermission, IsAdminUser, IsAnonymous]
+        return [permission() for permission in permission_classes]
 
     # @action(detail=True, methods=['GET'])
     def get_queryset(self):

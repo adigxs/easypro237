746f69aea5236cc4b9df70d94ac5e7c806e09a3c
tsdcloud <silatchomsiaka@gmail.com>
2024-04-06 12:06:11 +0200

    Update RequestViewSet.get_permissions()

--- request/views.py
+++ request/views.py
@@ -93,7 +93,7 @@ class RequestViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         permission_classes = []
         if self.action == 'list':
-            permission_classes = [HasCourierAgentPermission | HasRegionalAgentPermission | IsAdminUser]
+            permission_classes = [permissions.IsAuthenticated | HasCourierAgentPermission | HasRegionalAgentPermission | IsAdminUser]
             # permission_classes = [permissions.IsAuthenticated]
         if self.action == 'partial_update':
             permission_classes = [HasGroupPermission | IsAdminUser | IsAnonymous]

8e9b77277f7aa1f473135f1bc91928728b89a869
tsdcloud <tsd@bfclimited.com>
2024-05-10 13:51:42 +0200

    Improve dashboard.

--- visualization/dashboard.py
+++ visualization/dashboard.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from slugify import slugify
 
 from request.constants import PENDING, STARTED, COMPLETED, SHIPPED, RECEIVED, DELIVERED, REQUEST_STATUS, \
     DELIVERY_STATUSES
@@ -63,67 +64,67 @@ def render_dashboard(request, *args, **kwargs):
     queryset = Request.objects.all()
     total_count = queryset.count()
     output = dict()
-    # if court_name:
-    #     id_list = []
-    #     try:
-    #         if 'central' in court_name:
-    #             court = Court.objects.get(slug='minjustice-yaounde')
-    #         else:
-    #             court = Court.objects.get(slug='-'.join(slugify(court_name).split('-')[1:]))
-    #         agent = Agent.objects.get(court__id=court.id)
-    #         shipment_qs = Shipment.objects.filter(agent__id=agent.id)
-    #         for shipment in shipment_qs:
-    #             id_list.append(shipment.request.id)
-    #     except:
-    #         pass
-    #     queryset = queryset.filter(id__in=id_list)
-    #
-    # if municipality_name:
-    #     department_list = []
-    #     try:
-    #         municipality = Municipality.objects.get(slug=slugify(municipality_name))
-    #         department_list = [municipality.department.id]
-    #     except:
-    #         pass
-    #     queryset = queryset.filter(user_dpb__id__in=department_list)
-    # if department_name:
-    #     queryset = queryset.filter(user_dpb__slug=slugify(department_name))
-    # if region_name:
-    #     queryset = queryset.filter(user_dpb__region__slug=slugify(region_name))
-    # if period:
-    #     if period == "monthly":
-    #         created_on = datetime.now() - timedelta(days=28)
-    #     if period == "weekly":
-    #         created_on = datetime.now() - timedelta(days=7)
-    #     if period == "quarterly":
-    #         created_on = datetime.now() - timedelta(days=90)
-    #     if period == "semi-annually":
-    #         created_on = datetime.now() - timedelta(days=180)
-    #     if period == "annually":
-    #         created_on = datetime.now() - timedelta(days=365)
-    #     queryset = queryset.filter(created_on__gte=created_on)
-    #
-    # if created_on:
-    #     created_on = datetime.strptime(created_on, '%Y-%m-%d')
-    #     queryset = queryset.filter(created_on=created_on)
-    # if start_date and end_date:
-    #     start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    #     end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    #     if start_date > end_date or end_date > datetime.now():
-    #         queryset = queryset.filter(id__in=[])
-    #     queryset = queryset.filter(created_on__range=[start_date, end_date])
+    if court_name:
+        id_list = []
+        try:
+            if 'central' in court_name:
+                court = Court.objects.get(slug='minjustice-yaounde')
+            else:
+                court = Court.objects.get(slug='-'.join(slugify(court_name).split('-')[1:]))
+            agent = Agent.objects.get(court__id=court.id)
+            shipment_qs = Shipment.objects.filter(agent__id=agent.id)
+            for shipment in shipment_qs:
+                id_list.append(shipment.request.id)
+        except:
+            pass
+        queryset = queryset.filter(id__in=id_list)
+
+    if municipality_name:
+        department_list = []
+        try:
+            municipality = Municipality.objects.get(slug=slugify(municipality_name))
+            department_list = [municipality.department.id]
+        except:
+            pass
+        queryset = queryset.filter(user_dpb__id__in=department_list)
+    if department_name:
+        queryset = queryset.filter(user_dpb__slug=slugify(department_name))
+    if region_name:
+        queryset = queryset.filter(user_dpb__region__slug=slugify(region_name))
+    if period:
+        if period == "monthly":
+            created_on = datetime.now() - timedelta(days=28)
+        if period == "weekly":
+            created_on = datetime.now() - timedelta(days=7)
+        if period == "quarterly":
+            created_on = datetime.now() - timedelta(days=90)
+        if period == "semi-annually":
+            created_on = datetime.now() - timedelta(days=180)
+        if period == "annually":
+            created_on = datetime.now() - timedelta(days=365)
+        queryset = queryset.filter(created_on__gte=created_on)
+
+    if created_on:
+        created_on = datetime.strptime(created_on, '%Y-%m-%d')
+        queryset = queryset.filter(created_on=created_on)
+    if start_date and end_date:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        if start_date > end_date or end_date > datetime.now():
+            queryset = queryset.filter(id__in=[])
+        queryset = queryset.filter(created_on__range=[start_date, end_date])
     for request_status in REQUEST_STATUS:
-        queryset = Request.objects.filter(status=request_status[0])
-        output[request_status[0]] = {"requests": RequestListSerializer(queryset, many=True).data,
-                                     "count": queryset.filter(status=request_status[0]).count(),
+        qs = queryset.filter(status=request_status[0])
+        output[request_status[0]] = {"requests": RequestListSerializer(qs, many=True).data,
+                                     "count": qs.filter(status=request_status[0]).count(),
                                      "percentage": f"{queryset.filter(status=request_status[0]).count()/total_count * 100}%"}
     for request_status in DELIVERY_STATUSES:
         if request_status[0] == "STARTED":
             continue
         id_list = [shipment.request.id for shipment in Shipment.objects.filter(status__iexact=request_status[0])]
-        queryset = Request.objects.filter(id__in=id_list)
-        output[request_status[0]] = {"requests": RequestListSerializer(queryset, many=True).data,
-                                     "count": queryset.count(),
-                                     "percentage": f"{queryset.count() / total_count * 100}%"}
+        qs = queryset.filter(id__in=id_list)
+        output[request_status[0]] = {"requests": RequestListSerializer(qs, many=True).data,
+                                     "count": qs.count(),
+                                     "percentage": f"{qs.count() / total_count * 100}%"}
 
     return Response(output, status=status.HTTP_200_OK)

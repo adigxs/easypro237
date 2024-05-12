ff5b82e29ffcef74c621c180d68b235b1fe569d2
tsdcloud <tsd@bfclimited.com>
2024-05-12 09:10:11 +0200

    Create function render_agent_performances.

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from visualization.dashboard import render_dashboard, render_financial_report
+from visualization.dashboard import render_dashboard, render_financial_report, render_agent_performances
 
 app_name = 'visualization'
 
 urlpatterns = [
     path('render_dashboard/', render_dashboard, name='render_dashboard'),
     path('render_financial_report/', render_financial_report, name='render_financial_report'),
+    path('render_agent_performances/', render_agent_performances, name='render_agent_performances'),
 ]

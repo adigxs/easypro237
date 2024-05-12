8c859467b3d15768313fd47eb2c67072db30343e
tsdcloud <tsd@bfclimited.com>
2024-05-12 05:59:37 +0200

    Process financial report.

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from visualization.dashboard import render_dashboard
+from visualization.dashboard import render_dashboard, render_financial_report
 
 app_name = 'visualization'
 
 urlpatterns = [
     path('render_dashboard/', render_dashboard, name='render_dashboard'),
+    path('render_financial_report/', render_financial_report, name='render_financial_report'),
 ]
